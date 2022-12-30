fc2ceb9b6d9c7ab0cb3b0214d812d4bc0a880533
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2022-12-30 13:41:10 +0500

    on mouse over func added

--- src/Component/StoryView/StoryView.js
+++ src/Component/StoryView/StoryView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import ProgressBar from "../ProgressBar/ProgressBar";
 import { useEffect, useState } from "react";
 import fakeData from "../../fakeData";
@@ -10,61 +10,53 @@ const StoryView = () => {
   // const [list, setList] = useState(fakeData);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [color, setColor] = useState(0);
-
+  const storyRef = useRef();
+  var stoptimer;
 
   const stories = [
-    { id: 1, img: img1, per : 0 },
-    { id: 2, img: img2 , per : 0 },
-    { id: 3, img: img3 , per : 0},
+    { id: 1, img: img1, per: 0, dur: 3000 },
+    { id: 2, img: img2, per: 0, dur: 3000 },
+    { id: 3, img: img3, per: 0, dur: 3000 },
   ];
 
-  const [list , setList] = useState(stories)
-  
+  const [list, setList] = useState(stories);
+
+  useEffect(() => {
+     stoptimer = setTimeout(function() {
+      next();
+    }, list[currentIndex]?.dur);
+
+    return () => clearInterval(stoptimer)
 
 
-  
-  // useEffect(() => {
-  //   if (currentIndex !== list?.length) {
-  //     var a = setInterval(() => {
-  //       list[currentIndex].per += 100   
-  //     }, 100) 
-  //   } else {
-  //     clearInterval(a)
-  //   }
-    
-  // }, [currentIndex])
+  }, [currentIndex]);
 
-
-  // useEffect(() => {
-  //   if (currentIndex !== list?.length) {
-  //     var interval = setInterval(() => {
-  //       setCurrentIndex((currentIndex + 1) % stories.length);
-  //       }, 3000); // update every 3 seconds
-  //   }
-  //   else {
-  //     clearInterval(interval)
-  //   }
-
-  // }, [currentIndex]);
-
-  
-  
   const next = () => {
-    if (currentIndex <= list?.length) {
-      setCurrentIndex(prev => prev + 1)
-      list[currentIndex].per = 100
-  } else {
-    return false
+    if (currentIndex !== list?.length) {
+      setCurrentIndex((prev) => prev + 1);
+      list[currentIndex].per = 100;
+    } else {
+      return false;
     }
-  }
+  };
 
   const prev = () => {
-    if (currentIndex ===  0) {
+    if (currentIndex === 0) {
       return false;
     } else {
-      setCurrentIndex(prev => prev - 1)
-      list[currentIndex - 1].per = 0
+      setCurrentIndex((prev) => prev - 1);
+      list[currentIndex - 1].per = 0;
     }
+  };
+
+  const handleMouse = () => {
+     clearInterval(stoptimer)
+  };
+
+  const handleMouseOut = () => {
+    setTimeout(() => {
+      next();
+    },  list[currentIndex]?.dur)
   }
 
   return (
@@ -81,13 +73,33 @@ const StoryView = () => {
         </div>
 
         <div className="storyView_text">
-          {currentIndex === 0 ? "" : <button className="left" onClick={() => prev()}><i class="fa-solid fa-arrow-left"></i></button>}  
-          
+          {currentIndex === 0 ? (
+            ""
+          ) : (
+            <button className="left" onClick={() => prev()}>
+              <i class="fa-solid fa-arrow-left"></i>
+            </button>
+          )}
 
-          { currentIndex === list?.length ? <h2>End Of Story</h2> : <img src={list?.[currentIndex]?.img} alt="No" />}  
-          
-         
-         { currentIndex === list?.length ? "" : <button className="right" onClick={() => next() } ><i class="fa-solid fa-arrow-right"></i></button> } 
+          {currentIndex === list?.length ? (
+            <h2>End Of Story</h2>
+          ) : (
+          <img
+              onMouseOut={() => handleMouseOut() }      
+              onMouseOver={() => handleMouse()}
+              ref={storyRef}
+              src={list?.[currentIndex]?.img}
+              alt="No"
+            />
+          )}
+
+          {currentIndex === list?.length ? (
+            ""
+          ) : (
+            <button className="right" onClick={() => next()}>
+              <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          )}
         </div>
       </div>
     </div>
